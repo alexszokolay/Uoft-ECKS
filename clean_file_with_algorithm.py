e2d1d6cbd1a77665269cd999b59ec8e34d24760b
ikraskov <ivan.s.kraskov@gmail.com>
2023-01-21 20:12:39 -0500

    8 PM commit
    use the clean file with algorithm

--- clean_file_with_algorithm.py
+++ clean_file_with_algorithm.py
@@ -5,6 +5,7 @@ import csv
 import copy
 import random
 
+
 @dataclass
 class Interest:
     """An interest, like Art, Sports, Music, Skiing
@@ -39,27 +40,23 @@ class InterestsList:
         self.listy.append(Interest('Convention & Trade Centres', ['Boredom', 'Loneliness']))
         self.listy.append(Interest('Sports / Entertainment Venue', ['Sadness', 'Boredom', 'Loneliness']))
 
+
 @dataclass
 class TherapyTravel:
     """
         The main system class
+        Attributes:
+            - main_dict is the dictionary with 5 main emotions. It stores the results of the 15-question survey.
+
         >>> abc = TherapyTravel()
         >>> interest_List = InterestsList()
-        Representation Invariants:
-        -
     """
-    # anger: (str, int)
-    # boredom: (str, int)
-    # loneliness: (str, int)
-    # stress: (str, int)
-    # sadness: (str, int)
     main_dict: dict
 
     def __init__(self) -> None:
-        self.main_dict = {'Anger': 0, 'Boredom': 0,
-                          'Loneliness': 0, 'Stress': 0, 'Sadness': 0}
+        self.main_dict = {'Anger': 0, 'Boredom': 0, 'Loneliness': 0, 'Stress': 0, 'Sadness': 0}
 
-    def set_Emotions(self) -> None:
+    def set_Emotions_test(self) -> None:
         """
         Just a set method for testing purposes
         """
@@ -73,8 +70,8 @@ class TherapyTravel:
         """
         Method that returns two strongest emotions and the point difference between them or just the strongest emotion
         """
-        copy_of_dict = self.main_dict.copy()    #The copy was created so that the pop method below wouldn't
-                                                # mutate the original dict
+        # The copy was created so that the pop method below wouldn't mutate the original dictionary
+        copy_of_dict = self.main_dict.copy()
 
         max_key = max(copy_of_dict, key=copy_of_dict.get)
         st_value = (max_key, copy_of_dict[max_key])
@@ -84,7 +81,7 @@ class TherapyTravel:
         point_difference = st_value[1] - nd_value[1]
 
         if point_difference > 5:
-            return st_value[0]  # Anger or Sadness
+            return st_value[0]  # 'Anger' or 'Sadnessm' for example
         else:
             return st_value[0], nd_value[0]
 
